e3f5d42c7945087f27a9863c55a65a46d7133e31
kasia.kordys <kasia.kordys@gmail.com>
2026-03-16 21:19:52 +0200

    add client, styling packs and sample ui

--- API/Program.cs
+++ API/Program.cs
@@ -15,8 +15,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(connectionString);
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200", "https://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors("AllowAngular");
 app.MapControllers();
 
 app.Run();
