04ba025979a8140943401f383ed436ba276ffe61
kasia.kordys <kasia.kordys@gmail.com>
2026-03-29 23:55:41 +0300

    add and utilize member repository

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,31 +1,39 @@
-using API.Data;
+
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
 
-    public class MembersController(AppDbContext context) : BaseApiController
+    public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         [HttpGet]   // localhost:5001/api/members
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
-            var members = await context.Users.ToListAsync();
+            var members = await memberRepository.GetMembersAsync();
 
             return Ok(members);
         }
 
-        [Authorize]
-        [HttpGet("{id}")]   // localhost:5001/api/members/ewa-id
-        public async Task<ActionResult<AppUser>> GetMember(string id)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Member>> GetMember(string id)
         {
-            var member = await context.Users.FindAsync(id);
+            var member = await memberRepository.GetMemberByIdAsync(id);
 
             if (member == null) return NotFound();
 
             return Ok(member);
+        }
+
+        [Authorize]
+        [HttpGet("{id}/photos")]
+        public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
+        {
+            var photos = await memberRepository.GetPhotosForMemberAsync(id);
+
+            return Ok(photos);
         }
     }
 }
---
+++ API/Interfaces/IMemberRepository.cs
@@ -0,0 +1,10 @@
+using API.Entities;
+
+public interface IMemberRepository
+{
+    void Update(Member member);
+    Task<bool> SaveAllAsync();
+    Task<IReadOnlyList<Member>> GetMembersAsync();
+    Task<Member?> GetMemberByIdAsync(string id);
+    Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId);
+}
